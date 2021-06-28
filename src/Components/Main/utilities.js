9344af0bb651b71263ee220e91aee70aa8e4cb3f
Mohamed Sabith <47350620+sabith-nadakkavil@users.noreply.github.com>
2021-06-28 15:23:58 +0530

    Update utilities.js

--- src/Components/Main/utilities.js
+++ src/Components/Main/utilities.js
@@ -5,6 +5,9 @@ function printError(id, msg) {
 }
 
 export const validation = () => {
+  let regex = null;
+  let validated = true;
+
   const {
     name,
     age,
@@ -14,15 +17,14 @@ export const validation = () => {
     phoneNumber,
     proof
   } = dataStore.getUserInfo;
-  let regex = null;
-  let validated = true;
-  if (name === "") {
-    printError("nameError", "Name can't be empty");
-    validated = false;
-  }
+
   regex = /^[a-zA-Z\s]+$/;
   if (regex.test(name) === false) {
     printError("nameError", "Invalid name");
+    validated = false;
+  }
+  if (name === "") {
+    printError("nameError", "Name can't be empty");
     validated = false;
   }
   if (gender === "") {
@@ -49,23 +51,24 @@ export const validation = () => {
     printError("proofError", "Please select proof");
     validated = false;
   }
-  if (proofId === "") {
-    printError("proofIdError", "Proof ID can't be empty");
-    validated = false;
-  }
+
   regex = /^[a-zA-Z0-9]+$/;
   if (regex.test(proofId) === false) {
     printError("proofIdError", "Invalid Proof ID");
     validated = false;
   }
-  if (phoneNumber === "") {
-    printError("phoneError", "Phone Number can't be empty");
+  if (proofId === "") {
+    printError("proofIdError", "Proof ID can't be empty");
     validated = false;
   }
 
   regex = /^\d{10}$/;
   if (regex.test(phoneNumber) === false) {
     printError("phoneError", "Invalid phone number");
+    validated = false;
+  }
+  if (phoneNumber === "") {
+    printError("phoneError", "Phone Number can't be empty");
     validated = false;
   }
   return validated;
